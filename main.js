d150fc9abc2567ad6634bb724791c62a0d7fdaf9
Dane Powell <git@danepowell.com>
2016-01-17 20:33:47 -0600

    Trying a different approach.

--- main.js
+++ main.js
@@ -3,29 +3,38 @@
     // Handle elevators.
     _.each(elevators, function(elevator) {
       elevator.on("idle", function() {
-        var nextFloor = (elevator.currentFloor() + 1) % floors.length;
+        var nextFloor = Math.floor(floors.length / 2);
         elevator.goToFloor(nextFloor);
       });
       elevator.on("floor_button_pressed", function(floorNum) {
         elevator.goToFloor(floorNum);
       });
       elevator.on("passing_floor", function(floorNum, direction) {
-        var queue = elevator.destinationQueue;
-        // Need to do something with this queue...
-        elevator.destinationQueue = queue;
-        elevator.checkDestinationQueue();
+        // Should we stop at this floor?
+        // First, see if someone in the elevator wants to get off.
+        var pressedFloors = elevator.getPressedFloors();
+        if (pressedFloors.indexOf(floorNum) != 1) {
+          elevator.goToFloor(floorNum, true);
+        }
+        // Now see if someone on the floor is going in our direction.
+        var floor = floors[floorNum];
+        if ((floor.buttonStates.up && direction == "up") || (floor.buttonStates.down && direction == "down")) {
+          elevator.goToFloor(floorNum, true);
+        }
       });
     });
 
     // Handle floors.
     _.each(floors, function(floor) {
       floor.on("up_button_pressed", function() {
-        var elevatorIndex = Math.floor(Math.random() * elevators.length);
-        elevators[elevatorIndex].goToFloor(floor.floorNum());
+        _.each(elevators, function(elevator) {
+          elevator.goToFloor(floor.floorNum());
+        });
       })
       floor.on("down_button_pressed", function() {
-        var elevatorIndex = Math.floor(Math.random() * elevators.length);
-        elevators[elevatorIndex].goToFloor(floor.floorNum());
+        _.each(elevators, function(elevator) {
+          elevator.goToFloor(floor.floorNum());
+        });
       })
     });
   },
