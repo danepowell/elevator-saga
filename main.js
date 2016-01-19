e8554e703a44f025faff2f975c505dc9a9ba87b0
Dane Powell <git@danepowell.com>
2016-01-19 16:51:47 -0600

    Refactoring.

--- main.js
+++ main.js
@@ -2,35 +2,20 @@
   init: function(elevators, floors) {
     // Handle elevators.
     _.each(elevators, function(elevator) {
-      elevator.on("idle", function() {
-        // TODO: Look for more passengers in the direction I'm traveling.
-        var nextFloor = Math.floor(floors.length / 2);
-        elevator.goToFloor(nextFloor);
+      elevator.on("stopped_at_floor", function(floorNum) {
+        sortQueue(elevator);
       });
       elevator.on("floor_button_pressed", function(floorNum) {
         elevator.goToFloor(floorNum);
+        sortQueue(elevator);
       });
       elevator.on("passing_floor", function(floorNum, direction) {
-        // TODO: Set direction indicator.
-        // TODO: Check if someone on this floor is heading my direction and add to queue.
-        // TODO: Reorder queue according to direction I'm traveling.
-        // Should we stop at this floor?
-        // First, see if someone in the elevator wants to get off.
-        var pressedFloors = elevator.getPressedFloors();
-        if (pressedFloors.indexOf(floorNum) != 1) {
-          elevator.goToFloor(floorNum, true);
-        }
-        // Now see if someone on the floor is going in our direction.
-        var floor = floors[floorNum];
-        if ((floor.buttonStates.up && direction == "up") || (floor.buttonStates.down && direction == "down")) {
-          elevator.goToFloor(floorNum, true);
-        }
+        sortQueue(elevator);
       });
     });
 
     // Handle floors.
     _.each(floors, function(floor) {
-      // TODO: Don't do anything here? Unless an elevator is idle.
       floor.on("up_button_pressed", function() {
         _.each(elevators, function(elevator) {
           elevator.goToFloor(floor.floorNum());
@@ -42,6 +27,41 @@
         });
       })
     });
+
+    function sortQueue(elevator) {
+      // TODO: Rebuild this queue in a completely different way - go through
+      // each floor and see if it's requested and in the correct direction.
+      var floorNum = elevator.currentFloor();
+      if (elevator.goingUpIndicator()) {
+        var prograde = elevator.destinationQueue.filter(function(x) {return x >= floorNum;});
+        var retrograde = elevator.destinationQueue.filter(function(x) {return x < floorNum;});
+        prograde.sort(sortNumber);
+        retrograde.sort(sortNumber);
+        retrograde.reverse();
+        if (prograde.length == 0) {
+          elevator.goingUpIndicator(false);
+          elevator.goingDownIndicator(true);
+        }
+      }
+      else {
+        var prograde = elevator.destinationQueue.filter(function(x) {return x <= floorNum;});
+        var retrograde = elevator.destinationQueue.filter(function(x) {return x > floorNum;});
+        prograde.sort(sortNumber);
+        prograde.reverse();
+        retrograde.sort(sortNumber);
+        if (prograde.length == 0) {
+          elevator.goingUpIndicator(true);
+          elevator.goingDownIndicator(false);
+        }
+      }
+      elevator.destinationQueue = prograde.concat(retrograde);
+      elevator.checkDestinationQueue();
+    }
+
+    function sortNumber(a,b) {
+      return a - b;
+    }
+
   },
   update: function(dt, elevators, floors) {
     // We normally don't need to do anything here
