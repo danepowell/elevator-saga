0dcf3bd1b9cc2318fde12e588822a592e012ec50
Dane Powell <git@danepowell.com>
2016-01-20 17:50:43 -0600

    Refactoring.

--- main.js
+++ main.js
@@ -3,46 +3,42 @@
     // Handle elevators.
     _.each(elevators, function(elevator) {
       elevator.on("stopped_at_floor", function(floorNum) {
-        sortQueue(elevator);
+        sortQueue();
       });
       elevator.on("floor_button_pressed", function(floorNum) {
-        elevator.goToFloor(floorNum);
-        sortQueue(elevator);
+        sortQueue();
       });
       elevator.on("passing_floor", function(floorNum, direction) {
-        sortQueue(elevator);
+        sortQueue();
       });
     });
 
     // Handle floors.
     _.each(floors, function(floor) {
       floor.on("up_button_pressed", function() {
-        _.each(elevators, function(elevator) {
-          elevator.goToFloor(floor.floorNum());
-        });
+        sortQueue();
       })
       floor.on("down_button_pressed", function() {
-        _.each(elevators, function(elevator) {
-          elevator.goToFloor(floor.floorNum());
-        });
+        sortQueue();
       })
     });
 
-    function sortQueue(elevator) {
-      // TODO: Rebuild this queue in a completely different way - go through
-      // each floor and see if it's requested and in the correct direction.
-      var floorNum = elevator.currentFloor();
-      if (elevator.goingUpIndicator()) {
-        var prograde = elevator.destinationQueue.filter(function(x) {return x >= floorNum;});
-        var retrograde = elevator.destinationQueue.filter(function(x) {return x < floorNum;});
-        prograde.sort(sortNumber);
-        retrograde.sort(sortNumber);
-        retrograde.reverse();
-        if (prograde.length == 0) {
-          elevator.goingUpIndicator(false);
-          elevator.goingDownIndicator(true);
-        }
-      }
+    function sortQueue() {
+      _.each(elevators, function(elevator) {
+        // If no direction set, just go to the floor.
+        // Go through each floor and see if it's requested and in the correct direction.
+        /**         var floorNum = elevator.currentFloor();
+         if (elevator.goingUpIndicator()) {
+         var prograde = elevator.destinationQueue.filter(function(x) {return x >= floorNum;});
+         var retrograde = elevator.destinationQueue.filter(function(x) {return x < floorNum;});
+         prograde.sort(sortNumber);
+         retrograde.sort(sortNumber);
+         retrograde.reverse();
+         if (prograde.length == 0) {
+         elevator.goingUpIndicator(false);
+         elevator.goingDownIndicator(true);
+         }
+         }
       else {
         var prograde = elevator.destinationQueue.filter(function(x) {return x <= floorNum;});
         var retrograde = elevator.destinationQueue.filter(function(x) {return x > floorNum;});
@@ -56,7 +52,9 @@
       }
       elevator.destinationQueue = prograde.concat(retrograde);
       elevator.checkDestinationQueue();
-    }
+        **/
+      }
+            }
 
     function sortNumber(a,b) {
       return a - b;
